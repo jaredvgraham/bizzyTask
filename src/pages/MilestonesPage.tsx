80abdfc7b1dba40aa2ce71e6b3e743be3fb338fc
Jared Graham <jaredgraham@jareds-mbp.mynetworksettings.com>
2024-07-06 20:53:41 -0400

    fixed deployy

--- src/pages/MilestonesPage.tsx
+++ src/pages/MilestonesPage.tsx
@@ -5,12 +5,12 @@ import Milestones from "@/components/Milestones";
 import Sidebar from "@/components/Sidebar";
 
 const MilestonesPage = () => {
-  const { id } = useParams() as { id: string };
+  // const { id } = useParams() as { id: string };
 
   return (
     <div className="flex min-h-screen">
       <div className="flex-1 p-8 bg-slate-50 border-t-2 border-slate-300">
-        <Milestones businessId={id as string} />
+        <Milestones />
       </div>
     </div>
   );
