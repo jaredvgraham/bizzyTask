5cffeca8739357cf71a19f0beb98992c0828fdee
JaredvGraham <jgram153@icloud.com>
2024-07-09 15:15:55 -0400

    updated progress calculation

--- src/services/firebaseProgressService.ts
+++ src/services/firebaseProgressService.ts
@@ -1,5 +1,26 @@
 import { db } from "@/lib/firebaseAdmin";
 
+interface Description {
+  text: string;
+  createdAt: Date;
+  completed: boolean;
+}
+
+interface Task {
+  id: string;
+  name: string;
+  descriptions: Description[];
+  completed: boolean;
+  createdAt: FirebaseFirestore.Timestamp;
+}
+
+interface Category {
+  id: string;
+  name: string;
+  completed: boolean;
+  createdAt: FirebaseFirestore.Timestamp;
+}
+
 export const calculateProgress = async (
   businessId: string
 ): Promise<number> => {
@@ -9,13 +30,22 @@ export const calculateProgress = async (
       .doc(businessId)
       .collection("categories");
     const categoriesSnapshot = await categoriesRef.get();
-    const fetchedCategories = categoriesSnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    const fetchedCategories: Category[] = categoriesSnapshot.docs.map(
+      (doc) => ({
+        id: doc.id,
+        name: doc.data().name,
+        completed: doc.data().completed,
+        createdAt: doc.data().createdAt,
+      })
+    );
 
-    let totalTasks = 0;
-    let completedTasks = 0;
+    let totalItems = 0;
+    let completedItems = 0;
+
+    totalItems += fetchedCategories.length;
+    completedItems += fetchedCategories.filter(
+      (category) => category.completed
+    ).length;
 
     for (const category of fetchedCategories) {
       const tasksRef = db
@@ -25,18 +55,33 @@ export const calculateProgress = async (
         .doc(category.id)
         .collection("tasks");
       const tasksSnapshot = await tasksRef.get();
-      const tasks = tasksSnapshot.docs.map((taskDoc) => ({
+      const tasks: Task[] = tasksSnapshot.docs.map((taskDoc) => ({
         id: taskDoc.id,
-        ...taskDoc.data(),
-        completed: taskDoc.data().completed, // Ensure 'completed' property is included
+        name: taskDoc.data().name,
+        descriptions: taskDoc.data().descriptions,
+        completed: taskDoc.data().completed,
+        createdAt: taskDoc.data().createdAt,
       }));
 
-      totalTasks += tasks.length;
-      completedTasks += tasks.filter((task) => task.completed).length;
+      for (const task of tasks) {
+        totalItems += 1;
+        if (task.completed) {
+          completedItems += 1;
+        }
+
+        if (task.descriptions && Array.isArray(task.descriptions)) {
+          for (const description of task.descriptions) {
+            totalItems += 1;
+            if (description.completed) {
+              completedItems += 1;
+            }
+          }
+        }
+      }
     }
 
-    if (totalTasks > 0) {
-      return Math.round((completedTasks / totalTasks) * 100);
+    if (totalItems > 0) {
+      return Math.round((completedItems / totalItems) * 100);
     } else {
       return 0;
     }
